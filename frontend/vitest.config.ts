1e14790bb33535652a5903611fad2e884be35762
Adrian Martinez Granados <amg538@inlumine.ual.es>
2026-05-28 01:50:17 +0200

    fix

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,17 +1,5 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    reporters: process.env['CI']
-      ? ['verbose', ['junit', { outputFile: 'test-results/junit.xml' }]]
-      : ['verbose'],
-    coverage: {
-      reporter: ['text-summary', 'lcov', 'json', 'html'],
-      exclude: [
-        'src/app/**/*.spec.ts',
-        'src/app/**/*.routes.ts',
-        'src/app/**/*.config.ts',
-      ],
-    },
-  },
+  test: {},
 });
